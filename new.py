44841583577b27ce755587e1a8fdb4e7d3c56156
Taopaipai300 <taofeeksanni1991@gmail.com>
2022-12-09 11:12:21 +0000

    more work

---
+++ new.py
@@ -0,0 +1,26 @@
+# Numbers
+print(6 + 5)
+print(2 * 2)
+weight = 7
+height = 5
+print(weight * height)
+word = 'Python'
+print(word[1])
+print(word[1:4])
+print(word[1:4+1])
+print(word[-1])
+print(word[-1:0:-1])
+print(word[::-1])
+
+print(word[:2] + word[2:])
+
+# Fibonacci series: The sum of two elements defines the next
+a, b = 0, 1
+while a < 10:
+    print(a)
+    a, b = b, a+b
+
+c = 1
+while c < 5:
+    print(c)
+    c += 1
